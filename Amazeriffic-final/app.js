5780b045a6aa90e034ca00f5cb5ca5b435be114c
Pedro Tashima <23709916+Tashima42@users.noreply.github.com>
2020-07-08 18:43:08 -0300

    Add tab added

--- Amazeriffic-final/app.js
+++ Amazeriffic-final/app.js
@@ -1,6 +1,7 @@
 var main = function () {
     "use strict";
 
+    //List of things to show on the DOM
     var toDos = [
         "Finish Writing this book",
         "Finish the feedback of trainees",
@@ -9,25 +10,35 @@ var main = function () {
         "Kill a dog (with love)"
     ];
 
+    //Cicle thru the tabs
     $(".tabs span").toArray().forEach(function (element) {
         $(element).on("click", function () {
             var $element = $(element),
                 $content;
-
+            //Change active tab collor
             $(".tabs a span").removeClass("active");
             $element.addClass("active");
             $("main .content").empty();
 
+            //Select first tab
             if ($element.parent().is(":nth-child(1)")) {
-                console.log("parent 1");
-            } else if ($element.parent().is(":nth-child(2)")) {
+                $content = $("<ul>");
+                toDos.forEach(function (toDos) {
+                    $content.append($("<li>").text(toDos));
+                });
+                $("main .content").append($content);
+            } 
+            
+            else if ($element.parent().is(":nth-child(2)")) {
                 $content = $("<ul>");
                 toDos.forEach(function (toDos) {
                     $content.append($("<li>").text(toDos));
                 })
                 $("main .content").append($content);
 
-            } else if ($element.parent().is(":nth-child(3)")) {
+            } 
+            
+            else if ($element.parent().is(":nth-child(3)")) {
                 console.log("parent 3");
             }
 
